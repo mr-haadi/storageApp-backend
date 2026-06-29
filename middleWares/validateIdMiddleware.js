6a481441bea155ec3be458a6c05b6e28783e6285
Mir Haadi <saifraja1611@gmail.com>
2026-06-29 19:37:32 +0530

    remove mongodb import

--- middleWares/validateIdMiddleware.js
+++ middleWares/validateIdMiddleware.js
@@ -1,7 +1,7 @@
-import { ObjectId } from "mongodb";
+import { Types } from "mongoose";
 
 export default function (req, res, next, id) {
-  if (!ObjectId.isValid(id)) {
+  if (!Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: `Invalid Id! ${id}` });
   }
   next();
