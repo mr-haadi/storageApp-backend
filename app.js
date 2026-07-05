393a6cddbbd51b72bb87183c5089faf946735e1c
mr-haadi <haadi.exe@gmail.com>
2026-07-06 03:19:11 +0530

    add timestamp

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use("/webhook", webhookRoutes);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	return res.json({message: "Hello from mirhaadi cloud app, App is working fine in production.", timeStamp: Date.now().toLocaleString()});
+	return res.json({message: "Hello from mirhaadi cloud app, App is working fine in production.", timeStamp: new Date().toLocaleString()});
 });
 
 app.use("/", userRoutes);
